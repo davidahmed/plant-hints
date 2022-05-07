1efc4160fa81775910a4bb3c0a1f28d767d2491b
david.ahmed <davedshepherd@gmail.com>
2022-05-07 16:35:12 +0200

    Added helper components

---
+++ components/main-header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="px-4 sm:px-6 lg:px-16 py-8  text-gray-800 py-8">
+    <div>
+      <p class="text-4xl font-semibold">
+        PlantHints 🌼
+      </p>
+    </div>
+    <div class="text-lg text-gray-600 py-2 tracking-wide ">
+      <p>Some catchy phrase here</p>
+    </div>
+  </div>
+</template>
---
+++ components/search-box.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bg-white w-full py-2 justify-center flex flex-col items-center">
+    <div class="flex max-w-lg border-2 border-gray-700 rounded-xl p-2 items-center">
+      <magnifying-glass class="text-gray-500" small="true" />
+      <input v-model="searchTerm" class="w-64 text-gray-500 pl-2 pr-2 focus:outline-none w-full" placeholder="Search houseplant by weather, color, or properties" type="search" @change="search">
+    </div>
+    <p class="hidden">
+      {{ search() }}
+    </p>
+  </div>
+</template>
+<script>
+import Fuse from 'fuse.js'
+export default
+{
+	components: ['maginifying-glass'],
+	props: [
+		'source'
+	],
+	data () {
+		return {
+			fuseOptions: {
+				keys: ['name']
+			},
+			fuse: new Fuse(this.source, { keys: ['name', 'colors'] }),
+			searchTerm: ''
+		}
+	},
+	methods: {
+		search () {
+			return this.fuse.search(this.searchTerm)
+		}
+	}
+}
+</script>
