2468a5f327add58153d47f279722d72c8f0d3921
david.ahmed <davedshepherd@gmail.com>
2022-05-07 10:56:33 +0200

    Added 'content' module to read flower data

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial />
+  <div>
+    <Header />
+    <SearchBox />
+    <div class="flex bg-red-200">
+      <div v-for="plant in plants" :key="plant.slug">
+        <PlantCard :plant="plant" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+	name: 'IndexPage',
+	async asyncData ({ $content }) {
+		const plants = await $content('plants').only(['name', 'slug', 'img']).fetch()
+		return {
+			plants
+		}
+	}
 }
 </script>
