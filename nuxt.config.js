f2d9d23b6ad687f5d4ab7478d8f4f838df49b290
david.ahmed <davedshepherd@gmail.com>
2022-05-21 17:26:25 +0200

    Added proper title descriptions

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
-		title: 'plant-hints',
+		title: 'PlantHints- Find and take care of awesome houseplants',
 		htmlAttrs: {
 			lang: 'en'
 		},
